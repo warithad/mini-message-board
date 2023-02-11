57bd39a2b7a68c5532aadf81dddb0b7aa5b1466d
warith <warith.ad02@gmail.com>
2023-02-11 23:05:31 +0100

    Create files and implement code to add and display every new message with PUG and add runtime engine version to package.json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there',
+    user: 'Femi',
+    date: new Date()
+  },
+  {
+    text: 'Hi World',
+    user: 'Warith',
+    date: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form')
+})
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.messageText, user: req.body.messageUser, date: new Date()})
+  res.redirect('/')
+})
 module.exports = router;
